7fc93345b891ba520071c5e9687f0ee4cd980be7
Cyprian1999 <90463597+Cyprian1999@users.noreply.github.com>
2021-09-13 10:51:37 +0200

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -9,7 +9,7 @@ $email_body="User Name: $name.\n".
             "User Email: $visitor_email.\n".
             "Subject: $subject.\n".\
             "User Message: $message.\n";
-$to='cyprian.galicki@gmail.com';
+$to='cyprian.galicki@op.pl';
 $headers="From: $email_from \r\n";
 $headers.= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
