691fd7bbe543b91aba3e1c07f38ef6cf25939698
Cyprian1999 <90463597+Cyprian1999@users.noreply.github.com>
2021-09-15 12:30:17 +0200

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,16 +1,19 @@
 <?php
-  if (isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $visitor_email=$_POST['email'];
-    $subject=$_POST['subject'];
-    $message=$_POST['message'];
-
-    $mailTo="cyprian.galicki@op.pl";
-    $headers="From: ".$visitor_email";
-    $txt="You have received an e-mail from " .$name.".\n\n".$message;
-
-    mail($mailTo,$subject,$txt,$headers);
-    header("Locatation: contact.html");
+$name=$_Post['name'];
+$visitor_email=$_Post['_replyto'];
+$subject=$_Post['subject'];
+$message=$_Post['message'];
+$email_from= 'info@alfaiomegakursy.pl';
+$email_subject="New Form Submission";
+$email_body="User Name: $name.\n".
+            "User Email: $visitor_email.\n".
+            "Subject: $subject.\n".\
+            "User Message: $message.\n";
+$to='cyprian.galicki@gmail.com';
+$headers="From: $email_from \r\n";
+$headers.= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
   }
 ?>
 
